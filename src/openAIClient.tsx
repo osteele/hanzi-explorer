f5db3f6c75e98a4a2e3db27a93bb2733e7a34639
Oliver Steele <steele@osteele.com>
2023-04-23 20:15:35 +0800

    Remove unused material from openAIClient

--- src/openAIClient.tsx
+++ src/openAIClient.tsx
@@ -25,82 +25,6 @@ export type Completions = {
   tags?: string[];
 }[];
 
-export const CompletionModels = [
-  { value: "text-davinci-003", label: "Davinci 3" },
-  { value: "text-davinci-002", label: "Davinci 2" },
-  { value: "text-curie-001", label: "Curie" },
-  { value: "text-babbage-001", label: "Babbage" },
-  { value: "text-ada-001", label: "Ada" },
-];
-
-export const ChatModels = [{ value: "gpt-3.5-turbo", label: "GPT-3.5 Turbo" }];
-
-export type CompletionParameterName =
-  | "model"
-  | "prompt"
-  | "numResults"
-  | "maxTokens"
-  | "temperature"
-  | "frequencyPenalty"
-  | "presencePenalty"
-  | "topP";
-
-export const ParameterDescriptions: {
-  key: CompletionParameterName;
-  label: string;
-  description: string;
-  min: number;
-  max: number;
-}[] = [
-  {
-    key: "numResults",
-    label: "Number of Results",
-    description: "The number of completions to generate for each prompt.",
-    min: 1,
-    max: 10,
-  },
-  {
-    key: "maxTokens",
-    label: "Max Tokens",
-    description:
-      "The maximum number of tokens to generate. There are about two tokens per word.",
-    min: 100,
-    max: 4096,
-  },
-  {
-    key: "temperature",
-    label: "Temperature",
-    description:
-      "The higher the temperature, the more random the completions. Try 0.9 for more creative applications and 0.02 for a more focused asnwer to a question with a well-defined answer.",
-    min: 0,
-    max: 2,
-  },
-  {
-    key: "topP",
-    label: "Top P",
-    description:
-      "An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.",
-    min: 0,
-    max: 1,
-  },
-  {
-    key: "presencePenalty",
-    label: "Presence Penalty",
-    description:
-      "The higher the presence penalty, the more likely the model is to generate new tokens instead of repeating ones that have already been used at least once.",
-    min: 0,
-    max: 2,
-  },
-  {
-    key: "frequencyPenalty",
-    label: "Frequency Penalty",
-    description:
-      "The higher the frequency penalty, the more likely the model is to generate new tokens instead of repeating ones that have been frequently used.",
-    min: 0,
-    max: 2,
-  },
-];
-
 export async function getCompletions({
   apiKey,
   onProgress,
@@ -115,7 +39,6 @@ export async function getCompletions({
   temperature: number;
   frequencyPenalty: number;
   presencePenalty: number;
-  // topP: number;
 }): Promise<{ data: { choices: Completions } }> {
   // if prompt is an array, apply streamingCompletion to each prompt
   const { numResults, prompt } = modelParameters;
@@ -147,7 +70,7 @@ export async function getCompletions({
 
   const isChatModel = modelParameters.model.startsWith("gpt-");
   const endpoint = isChatModel ? "chat/completions" : "completions";
-  const alteredModelParameters = isChatModel
+  const preparedModelParameters = isChatModel
     ? {
         messages: [
           {
@@ -167,7 +90,7 @@ export async function getCompletions({
     },
     body: JSON.stringify({
       stream: true,
-      ...renameModelParameterNames(alteredModelParameters),
+      ...renameModelParameterNames(preparedModelParameters),
     }),
   });
   if (response.status >= 400) {
@@ -212,26 +135,6 @@ export async function getCompletions({
     }
   }
   reader.releaseLock();
-  return { data: { choices } };
-}
-
-export async function combineCompletions({
-  apiKey,
-  parameterSets,
-}: {
-  apiKey: string;
-  parameterSets: (Omit<CompletionParameters, "prompt"> & {
-    prompt: string | string[];
-  })[];
-}): Promise<{
-  data: { choices: Completions };
-}> {
-  const responses = await Promise.all(
-    parameterSets.map((parameters) => getCompletions({ apiKey, ...parameters }))
-  );
-  const choices = responses.flatMap((r, i) =>
-    r.data.choices.map((c) => ({ ...c, tags: [`settings #${i + 1}`] }))
-  );
   return { data: { choices } };
 }
 
